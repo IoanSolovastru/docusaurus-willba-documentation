027e985ce1003ba4ef5d654c503202348de30625
Ioan Solovastru <nelusolovastru@gmail.com>
2023-02-02 12:44:26 +0200

    stable release

---
+++ docs/sales/temp_mbHBstxf.md
@@ -0,0 +1,18 @@
+---
+sidebar_position: 3
+---
+
+# Temp
+
+Download things
+
+```plain
+This a code block
+```
+
+| Test | Test1 | Test2 |
+| ---- | ----- | ----- |
+|      |       |       |
+|      |       |       |
+
+Test
---
+++ docusaurus.config.js
@@ -0,0 +1,141 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Willba doc",
+  tagline: "Willba doc",
+  favicon: "img/willba.png",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-test-site.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "willba", // Usually your GitHub org/user name.
+  projectName: "willba-doc", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fi"],
+    localeConfigs: {
+      en: { label: "English" },
+      fi: { label: "Finnish" },
+    },
+  },
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          breadcrumbs: true,
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        blog: false,
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      forceDarkMode: true,
+      darkMode: true,
+      colorMode: {
+        // "light" | "dark"
+        defaultMode: "dark",
+      },
+      // Replace with your project's social card
+      image: "img/willba.png",
+      docs: {
+        sidebar: {
+          autoCollapseCategories: false,
+        },
+      },
+      navbar: {
+        // title: "Docs",
+        logo: {
+          alt: "Willba logo",
+          src: "img/willba.png",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            href: "https://github.com/facebook/docusaurus",
+            label: "GitHub",
+            position: "right",
+          },
+          {
+            type: "localeDropdown",
+            position: "right",
+            dropdownItemsBefore: [],
+            className: "icon-link language navbar__item",
+          },
+          // versioning
+          // {
+          //   type: "docsVersionDropdown",
+          //   position: "right",
+          // },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Willba documentation.`,
+      },
+      prism: {
+        theme: { plain: {}, styles: [] },
+      },
+    }),
+};
+
+module.exports = config;
