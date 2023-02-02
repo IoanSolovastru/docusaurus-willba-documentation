812f9056dbff74d7b6db54117e24e09c5029b4a1
Ioan Solovastru <nelusolovastru@gmail.com>
2023-02-02 12:52:44 +0200

    update settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: "img/willba.png",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://docusaurus-willba-documentation.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "willba", // Usually your GitHub org/user name.
-  projectName: "willba-doc", // Usually your repo name.
+  organizationName: "IoanSolovastru", // Usually your GitHub org/user name.
+  projectName: "docusaurus-willba-documentation", // Usually your repo name.
+  deploymentBranch: "main",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
